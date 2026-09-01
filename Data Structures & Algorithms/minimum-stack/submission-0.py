0519c2229d92801454ade18494a1796bbb823854
May <166410836+MayZKyi25@users.noreply.github.com>
2026-09-01 04:25:16 -0400

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,28 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []       # stores all values
+        self.min_stack = []   # stores the minimum values
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+        # If min_stack is empty OR val is <= current minimum,
+        # save val as a new minimum
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        # If the value being removed is also the current minimum,
+        # remove it from min_stack too
+        if self.stack[-1] == self.min_stack[-1]:
+            self.min_stack.pop()
+
+        self.stack.pop()
+
+    def top(self) -> int:
+        # Last value in normal stack
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        # Last value in min_stack is always the current minimum
+        return self.min_stack[-1]
