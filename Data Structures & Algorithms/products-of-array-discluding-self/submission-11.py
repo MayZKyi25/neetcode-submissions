83fc252976ba196f463dd3ccf7e71eab4e5241af
May <166410836+MayZKyi25@users.noreply.github.com>
2026-09-15 19:44:31 -0400

    Add: products-of-array-discluding-self - submission-11

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-11.py
@@ -0,0 +1,35 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # num:  1   2   4   6 
+        # prod = 1      # store previous prod and update to res [] 
+        # calculating next prod = nums[i] * previous prod = nums[0] * 1 = 1*1 = 1
+
+        prod = 1
+        res = [1] * len(nums) # store left, then multiply directly with right prod
+
+        for i in range(len(nums)):
+            res[i] = prod
+            prod *= nums[i]
+        
+        # resetting the prod to 1 
+        prod = 1
+        for i in range(len(nums)-1,-1,-1):
+            # Take the left product already stored in res[i] and multiply it by the current right product.
+            res[i] *= prod 
+            # Include the current number so right_product is ready for the next index to the left.
+            prod *= nums[i] 
+        return res
+            
+        # 1 2 4 6
+
+        # 1    1   2   8
+        # 48  24   6   1
+        # what's the right arr and * left 
+
+         
+         
+    
+
+      
+
+        
